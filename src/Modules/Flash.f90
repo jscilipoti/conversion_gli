a0268881ad3c2081ed415d069cabc2b9c95af064
Juan Pablo Rovezzi <juanrovezzi@hotmail.com>
2023-10-18 16:31:40 -0300

    Function y Leer Bases

--- src/Modules/Flash.f90
+++ src/Modules/Flash.f90
@@ -6,8 +6,3 @@ module flash
     
 endmodule flash
     
-module flashout
-    real*8::ksalida(3)
-    real*8::agam(10,4),compfases(10,4)
-    integer::cont
-endmodule flashout
---
+++ src/Modules/Flashout.f90
@@ -0,0 +1,5 @@
+module flashout
+    real*8::ksalida(3)
+    real*8::agam(10,4),compfases(10,4)
+    integer::cont
+endmodule flashout
