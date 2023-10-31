a486f8965667f1b3c75a4e08cc80f8a1484e2028
Juan Pablo Rovezzi <juanrovezzi@hotmail.com>
2023-10-31 18:38:34 -0300

    Modernizacion

--- src/LLECALAS/Fuctions_SUB/ab_ban.f90
+++ src/LLECALAS/Fuctions_SUB/ab_ban.f90
@@ -1,45 +1,44 @@
 subroutine ab_ban1(model)
     !-------------------------------------------------------------------
-    !      Esta subrunina ABRE los bancos de datos siguientes:
-    !     - INTRCN.MDS      (UNIT=13)
-    !     - INTRCNAS.MDS    (UNIT=13)
-    !     - GRUPOSRAM.MDS   (UNIT=14)
-    !     - PARVOLAS.MDS    (UNIT=15)  
-    !     - PARENEAS.MDS    (UNIT=16)     
+    !   This subroutine opens each database:
+    !   - INTRCN.MDS      (UNIT=13)
+    !   - INTRCNAS.MDS    (UNIT=13)
+    !   - GRUPOSRAM.MDS   (UNIT=14)
+    !   - PARVOLAS.MDS    (UNIT=15)  
+    !   - PARENEAS.MDS    (UNIT=16)     
     !-------------------------------------------------------------------
     !
-        !use input_data, only:model
-          COMMON/AS/ASOC
-          LOGICAL ASOC
-          integer::model, mod
-       
-          
-        !CALL MODEL(mod)
-        if(model /= 3)then
-            if(model==1)then
-                open (unit=13,file='src/database/intrcn.mds',status='old',&
-                      access='direct',form='formatted',recl=850)   
-            else     
-                open (unit=13,file='src/database/intrcnas.mds',status='old',&
-                      access='direct',form='formatted',recl=850)        
-            endif
-            open (unit=14,file='src/database/gruposram.mds',status='old',&
-                   access='direct',form='formatted',recl=300)
-            open (unit=15,file='src/database/parvolas.mds',status='old',&
-                  access='direct',form='formatted',recl=850)
-            open (unit=16,file='src/database/pareneas.mds',status='old',&
-                  access='direct',form='formatted',recl=850)    
-        else
+    use iso_fortran_env, only: int8
+
+    implicit none
     
-            open (unit=14,file='src/database/gruposramgc.mds',status='old',&
-                   access='direct',form='formatted',recl=263)
-            open (unit=13,file='src/database/intrcngcalpha.mds',status='old',&
-                  access='direct',form='formatted',recl=730)
-            open (unit=16,file='src/database/intrcngckapa.mds',status='old',&
-                  access='direct',form='formatted',recl=730)    
-            
+    integer(kind=int8), intent(in) :: model
+
+    if(model /= 3) then
+        if(model == 1) then !if model = 1(UNIQUAC)
+            open (unit=13, file='src/database/intrcn.mds', status='old',&
+            access='direct', form='formatted', recl=850)   
+        else !if model = 0 (UNIFAC) or 2(A-UNIFAC)    
+            open (unit=13, file='src/database/intrcnas.mds', status='old',&
+            access='direct',form='formatted',recl=850)        
         endif
-          
+
+        !if model = 0(A-UNIFAC) or 1(UNIQUAC) or 2(A-UNIFAC)
+        open (unit=14, file='src/database/gruposram.mds', status='old', &
+            access='direct',form='formatted',recl=300)
+        open (unit=15, file='src/database/parvolas.mds', status='old', &
+            access='direct', form='formatted', recl=850)
+        open (unit=16, file='src/database/pareneas.mds', status='old', &
+            access='direct', form='formatted', recl=850)
+
+    else !if model = 3(GC)
+        open (unit=14, file='src/database/gruposramgc.mds', status='old',&
+            access='direct', form='formatted', recl=263)
+        open (unit=13, file='src/database/intrcngcalpha.mds', status='old',&
+            access='direct', form='formatted', recl=730)
+        open (unit=16, file='src/database/intrcngckapa.mds', status='old',&
+            access='direct', form='formatted', recl=730)    
+    endif
+    return
     
-          return
-    endsubroutine ab_ban1
+    end subroutine ab_ban1
